e5fb74e7007571053ecf99f70072a4985a25f96d
SMNoushan <noushan.md@gmail.com>
2024-12-19 23:26:04 +0600

    added: validate doc (check if doc exists, handle err and return res)

---
+++ src/app/utils/validateDoc.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-unused-vars */
+import status from 'http-status';
+import { Model } from 'mongoose';
+import AppError from '../errors/AppError';
+
+interface ValidateDocOptionsBase<T> {
+  model: Model<T>;
+  query: Record<string, unknown>;
+  errMsg?: string;
+  select?: string;
+  trueValidate?: boolean;
+}
+
+// Overloads
+function validateDoc<T>(
+  options: ValidateDocOptionsBase<T> & { trueValidate?: true },
+): Promise<T>;
+function validateDoc<T>(
+  options: ValidateDocOptionsBase<T> & { trueValidate: false },
+): Promise<null>;
+
+/**
+ * Validates the existence (or non-existence) of a document in the database.
+ *
+ * @template T - Type of the document.
+ * @param {ValidateDocOptionsBase<T>} options - Validation options.
+ * @returns {Promise<T | null>} - Returns the document if found or null.
+ * @throws {AppError} - Throws an error if validation fails.
+ */
+
+async function validateDoc<T>({
+  model,
+  query,
+  errMsg = '',
+  trueValidate = true,
+  select = '',
+}: ValidateDocOptionsBase<T>): Promise<T | null> {
+  const doc = await model.findOne(query).select(select);
+
+  if (trueValidate && !doc)
+    throw new AppError(
+      status.NOT_FOUND,
+      errMsg || 'Ref/Document does not exist.',
+    );
+
+  if (!trueValidate && doc)
+    throw new AppError(
+      status.CONFLICT,
+      errMsg || 'Ref/Document already exists.',
+    );
+
+  return doc as T | null;
+}
+
+export default validateDoc;
