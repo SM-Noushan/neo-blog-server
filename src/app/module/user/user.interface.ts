ddea359ba2274c119e2b2e2305f142d946144195
SMNoushan <noushan.md@gmail.com>
2024-12-20 00:49:03 +0600

    add: user interface

---
+++ src/app/module/user/user.interface.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+import { UserRole } from './user.constant';
+import { TLoginUser } from '../auth/auth.interface';
+
+export type TUserRole = keyof typeof UserRole;
+
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  role?: TUserRole;
+  isBlocked?: boolean;
+}
+
+export interface IValidateUserOptions {
+  payload: TLoginUser;
+  checkIsBlocked?: boolean;
+  checkIsPasswordMatched?: boolean;
+}
+
+// custom methods here
+export interface IUserModel extends Model<IUser> {
+  isUserExistsByEmail(
+    email: string,
+    throwErrorIfUserExist?: boolean,
+  ): Promise<IUser>;
+  isUserBlocked: (user: IUser) => Promise<void>;
+  isPasswordMatched: (user: IUser, password: string) => Promise<void>;
+  validateUser: ({
+    payload,
+    checkIsBlocked,
+    checkIsPasswordMatched,
+  }: IValidateUserOptions) => Promise<IUser>;
+}
